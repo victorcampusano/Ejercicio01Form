e85814f7e4c1b9b14b46d966aee512c0df2c06b8
Riviutec <riviutec@gmail.com>
2025-10-29 22:15:31 -0400

    Ejemplo de convertir un programa de consola a Windows Forms

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Ejercicio01Form
+{
+    partial class frmEjercicio1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHorasTrabajadas = new System.Windows.Forms.TextBox();
+            this.txtTarifa = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelSalario = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtHorasTrabajadas
+            // 
+            this.txtHorasTrabajadas.Location = new System.Drawing.Point(64, 61);
+            this.txtHorasTrabajadas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtHorasTrabajadas.Name = "txtHorasTrabajadas";
+            this.txtHorasTrabajadas.Size = new System.Drawing.Size(162, 28);
+            this.txtHorasTrabajadas.TabIndex = 0;
+            // 
+            // txtTarifa
+            // 
+            this.txtTarifa.Location = new System.Drawing.Point(64, 130);
+            this.txtTarifa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtTarifa.Name = "txtTarifa";
+            this.txtTarifa.Size = new System.Drawing.Size(162, 28);
+            this.txtTarifa.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 184);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Horas Trabajadas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tarifa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(279, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Su salario es:";
+            // 
+            // labelSalario
+            // 
+            this.labelSalario.AutoSize = true;
+            this.labelSalario.Font = new System.Drawing.Font("Source Sans Pro", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSalario.Location = new System.Drawing.Point(283, 98);
+            this.labelSalario.Name = "labelSalario";
+            this.labelSalario.Size = new System.Drawing.Size(0, 60);
+            this.labelSalario.TabIndex = 6;
+            // 
+            // frmEjercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 314);
+            this.Controls.Add(this.labelSalario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTarifa);
+            this.Controls.Add(this.txtHorasTrabajadas);
+            this.Font = new System.Drawing.Font("Source Sans Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmEjercicio1";
+            this.Text = "Ejercicio 1 - Ejemplo de Formularios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHorasTrabajadas;
+        private System.Windows.Forms.TextBox txtTarifa;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelSalario;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio01Form
+{
+    public partial class frmEjercicio1 : Form
+    {
+        public frmEjercicio1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double tarifa, horastrabajadas, horasextra, salario, tarifaextra;
+            //Console.WriteLine("ingrese horas trabajadas");
+            horastrabajadas = double.Parse(txtHorasTrabajadas.Text);
+            Console.WriteLine("ingrese tarifa");
+            tarifa = double.Parse(txtTarifa.Text);
+            if (horastrabajadas <= 40 && horastrabajadas >= 0)
+            {
+                salario = horastrabajadas * tarifa;
+                labelSalario.Text = salario.ToString();
+                //Console.WriteLine("el salario es: " + salario);
+            }
+            else if (horastrabajadas > 40)
+            {
+                horasextra = horastrabajadas - 40;
+                tarifaextra = tarifa + 0.5 * tarifa;
+                salario = horasextra * tarifaextra + 40 * tarifa;
+                labelSalario.Text = salario.ToString();
+                //Console.WriteLine("el salario es: " + salario);
+            }
+            else
+            {
+                MessageBox.Show("las horas trabajadas no pueden ser negativas");
+                //Console.WriteLine("las horas trabajadas no pueden ser negativas");
+
+            }
+    
+
+        }
+    }
+}
